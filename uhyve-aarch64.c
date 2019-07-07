cb59fb509a1c9135880cd096bcd4053ea652fc74
Jens Breitbart <jbreitbart@gmail.com>
2019-07-07 12:56:35 +0200

    `load_migration_data` is not returning any value. Change to void return value.

--- uhyve-aarch64.c
+++ uhyve-aarch64.c
@@ -29,36 +29,34 @@
 
 #define _GNU_SOURCE
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sched.h>
-#include <signal.h>
-#include <limits.h>
-#include <pthread.h>
-#include <semaphore.h>
 #include <elf.h>
 #include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <linux/const.h>
+#include <linux/kvm.h>
 #include <poll.h>
-#include <sys/wait.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
+#include <pthread.h>
+#include <sched.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/eventfd.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/time.h>
-#include <sys/eventfd.h>
-#include <linux/const.h>
-#include <linux/kvm.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-#include "uhyve.h"
 #include "proxy.h"
+#include "uhyve.h"
 
 #define GUEST_OFFSET		0x0
 
@@ -270,7 +268,7 @@ int load_checkpoint(uint8_t* mem, char* path)
 	err(1, "Checkpointing is currently not supported!");
 }
 
-int load_migration_data(uint8_t* mem)
+void load_migration_data(uint8_t* mem)
 {
 	err(1, "Checkpointing is currently not supported!");
 }
